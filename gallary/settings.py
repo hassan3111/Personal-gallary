19d733b46dd99a82add38819606a1a0bc4402c5b
hassan3111 <ali.hassan@student.moringaschool.com>
2021-11-30 14:54:58 +0300

    add dj_database import

--- gallary/settings.py
+++ gallary/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import django_heroku
+import dj_database_url
+from decouple import config,Csv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
