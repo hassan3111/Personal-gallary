bec189fa6d9ab293f6ea8e974119f8028b690b02
hassan3111 <ali.hassan@student.moringaschool.com>
2021-11-30 13:26:30 +0300

    add initial deployment

--- gallary/settings.py
+++ gallary/settings.py
@@ -58,7 +58,7 @@ SECRET_KEY = 'django-insecure-adtjy*x!f#k_$fv5g-xgp4zuzzx95fh82d1ba!a-4r(4m-78vl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS='.localhost','.herokuapp.com','.127.0.0.1'
 
 
 # Application definition
