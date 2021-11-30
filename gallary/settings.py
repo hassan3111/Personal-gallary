120c79df1498806fe39b26997edf33a91022f509
hassan3111 <ali.hassan@student.moringaschool.com>
2021-11-30 13:20:58 +0300

    add initial deployment

--- gallary/settings.py
+++ gallary/settings.py
@@ -53,6 +53,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-adtjy*x!f#k_$fv5g-xgp4zuzzx95fh82d1ba!a-4r(4m-78vl'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
 
 # Application definition
 
