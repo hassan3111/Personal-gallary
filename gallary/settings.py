f3e33660c292e6c7741e6bb5f7c853334f736943
hassan3111 <ali.hassan@student.moringaschool.com>
2021-11-30 14:04:41 +0300

    add initial deployment

--- gallary/settings.py
+++ gallary/settings.py
@@ -33,6 +33,17 @@ if config('MODE')=="dev":
        }
        
    }
+else:
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+       )
+   }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
